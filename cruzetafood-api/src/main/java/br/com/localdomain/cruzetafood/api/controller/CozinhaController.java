21b2108c4aaf8bb6deaa6ec5eed825be4580457f
André Gustavo <andre@cruzeta.com.br>
2021-02-16 22:44:11 -0300

    1 - Nova Implementação da classe de excessão, 2 - Modificação do Service para uso das validações de negócio, 3 - Ajuste na classe de controller para adaptação ao novo formato.

--- cruzetafood-api/src/main/java/br/com/localdomain/cruzetafood/api/controller/CozinhaController.java
+++ cruzetafood-api/src/main/java/br/com/localdomain/cruzetafood/api/controller/CozinhaController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.localdomain.cruzetafood.domain.exception.EntidadeEmUsoException;
 import br.com.localdomain.cruzetafood.domain.exception.EntidadeNaoEncontradaException;
@@ -39,12 +40,8 @@ public class CozinhaController {
 	}
 	
 	@GetMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-		Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
-		if (cozinha.isPresent()) {
-			return ResponseEntity.ok(cozinha.get());
-		}
-		return ResponseEntity.notFound().build();
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
+		return cadastroCozinha.buscarOuFalhar(cozinhaId);
 	}
 	
 	@PostMapping
@@ -54,27 +51,20 @@ public class CozinhaController {
 	}
 	
 	@PutMapping("/{cozinhaId}")	
-	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
-		Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
-		
-		if (cozinhaAtual.isPresent()) {
-			//Copiando todas as propriedades de um objeto para o outro, ignorando o primeiro atribulto "id"
-			BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id");
-			Cozinha cozinhaSalva = cadastroCozinha.salvar(cozinhaAtual.get());
-			return ResponseEntity.ok(cozinhaSalva);
-		}
-		return ResponseEntity.notFound().build();
+	public Cozinha atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cadastroCozinha.buscarOuFalhar(cozinhaId);
+		BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+		return cadastroCozinha.salvar(cozinhaAtual);
 	}
 	
+	
 	@DeleteMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long cozinhaId) {
 		try {
 			cadastroCozinha.excluir(cozinhaId);
-			return ResponseEntity.noContent().build();
 		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.notFound().build();
-		} catch (EntidadeEmUsoException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Não existe um cadastro de cozinha com o código: %d", cozinhaId));
 		}
 	}
 }
--- cruzetafood-api/src/main/java/br/com/localdomain/cruzetafood/domain/service/CadastroCozinhaService.java
+++ cruzetafood-api/src/main/java/br/com/localdomain/cruzetafood/domain/service/CadastroCozinhaService.java
@@ -3,7 +3,9 @@ package br.com.localdomain.cruzetafood.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.localdomain.cruzetafood.domain.exception.EntidadeEmUsoException;
 import br.com.localdomain.cruzetafood.domain.exception.EntidadeNaoEncontradaException;
@@ -13,6 +15,9 @@ import br.com.localdomain.cruzetafood.domain.repository.CozinhaRepository;
 @Service
 public class CadastroCozinhaService {
 
+	private static final String MENSAGEM_COZINHA_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
+	private static final String MENSAGEM_COZINHA_NAO_ENCONTRADA = "Não existe um cadastro de cozinha com o código: %d";
+	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
@@ -24,11 +29,18 @@ public class CadastroCozinhaService {
 		try {
 			cozinhaRepository.deleteById(cozinhaId);
 		} catch (EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(
-					String.format("Não existe um cadastro de cozinha com o código: %d", cozinhaId));
+			
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format(MENSAGEM_COZINHA_NAO_ENCONTRADA, cozinhaId));
+			
+//			throw new EntidadeNaoEncontradaException(
+//					String.format("Não existe um cadastro de cozinha com o código: %d", cozinhaId));
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-					String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
+					String.format(MENSAGEM_COZINHA_EM_USO, cozinhaId));
 		}	
+	}
+	
+	public Cozinha buscarOuFalhar(Long cozinhaId) {
+		return cozinhaRepository.findById(cozinhaId).orElseThrow( () -> new EntidadeNaoEncontradaException(String.format(MENSAGEM_COZINHA_NAO_ENCONTRADA, cozinhaId)));
 	}
 }
