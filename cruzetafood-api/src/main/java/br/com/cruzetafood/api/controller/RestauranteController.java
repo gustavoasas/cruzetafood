b1e104fc6dd18fc531d92a240a59c955c3b7cc30
André Gustavo <andre@cruzeta.com.br>
2021-03-16 22:01:31 -0300

    Adaptação para interceptar erros de patch erros de digitação no body.

--- cruzetafood-api/src/main/java/br/com/cruzetafood/api/controller/RestauranteController.java
+++ cruzetafood-api/src/main/java/br/com/cruzetafood/api/controller/RestauranteController.java
@@ -5,12 +5,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.reactive.ServletHttpHandlerAdapter;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.cruzetafood.domain.exception.EntidadeNaoEncontradaException;
@@ -102,12 +109,12 @@ public class RestauranteController {
 
 	@PatchMapping("/{restauranteId}")
 	@Deprecated
-	public ResponseEntity<?> aualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+	public ResponseEntity<?> aualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos, HttpServletRequest request) {
 		Optional<Restaurante> restauranteAtual = repository.findById(restauranteId);
 		if (!restauranteAtual.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
-		merge(campos, restauranteAtual.get());
+		merge(campos, restauranteAtual.get(), request);
 		//return atualizar(restauranteId, restauranteAtual.get());
 		return null;
 	}
@@ -123,18 +130,24 @@ public class RestauranteController {
 	 * 
 	 * @Author André Gustavo
 	 */
-	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-		ObjectMapper objectMapper = new ObjectMapper();
-		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
-
-		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-			field.setAccessible(true);
-
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-			System.out.println(nomePropriedade + " - " + valorPropriedade);
-
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
-		});
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
+		ServletServerHttpRequest servletServerHttpRequest = new ServletServerHttpRequest(request);
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+				field.setAccessible(true);
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+				System.out.println(nomePropriedade + " - " + valorPropriedade);
+	
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			});
+		} catch (IllegalArgumentException ex) {
+			Throwable rootCause = ExceptionUtils.getRootCause(ex);
+			throw new HttpMessageNotReadableException(ex.getMessage(), rootCause, servletServerHttpRequest);
+		}
 	}
 }
