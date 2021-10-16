87fbc5786aa5a3a83743df20bf9698dbe5dfac80
André Gustavo <andre@cruzeta.com.br>
2021-10-16 14:57:23 -0300

    teste adicionando

---
+++ cruzetafood-api/src/main/java/br/com/cruzetafood/domain/model/Cidade.java
@@ -0,0 +1,46 @@
+package br.com.cruzetafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
+import br.com.cruzetafood.core.validation.Groups;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 
+ * @Author André Gustavo
+ * 
+ */
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cidade {
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Column(nullable = false)
+	private String nome;
+	
+	@Valid
+	@ConvertGroup(from = Default.class, to = Groups.EstadoId.class)
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "estado_id", nullable = false)
+	private Estado estado;
+}
