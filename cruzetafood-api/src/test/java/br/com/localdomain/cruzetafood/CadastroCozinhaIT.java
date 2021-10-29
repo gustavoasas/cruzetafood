32feeaa9feafb456b3290d7e4c8084a778bfefee
André Gustavo <andre@cruzeta.com.br>
2021-10-28 22:26:51 -0300

    Problema com "org.hamcrest.Matchers" ao executar os testes. #1
    Resolvendo problema com a configuração do JUnit5. Foi adicionada uma biblioteca manualmente no BuildPath e resolveu o problema.

--- cruzetafood-api/src/test/java/br/com/localdomain/cruzetafood/CadastroCozinhaIT.java
+++ cruzetafood-api/src/test/java/br/com/localdomain/cruzetafood/CadastroCozinhaIT.java
@@ -1,8 +1,12 @@
 package br.com.localdomain.cruzetafood;
 
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -12,17 +16,43 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = CruzetafoodApiApplication.class)
-@SpringBootTest(properties = "spring.main.lazy-initialization=true", classes = CadastroCozinhaIntegrationTests.class)
+@ContextConfiguration(classes = {CruzetafoodApiApplication.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CadastroCozinhaIT {
+	
+	@LocalServerPort
+	public int port;
+	
+	@Autowired
+	private Flyway flyway;
+	
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+		
+		//flyway.migrate();
+	}
 	
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
 		RestAssured.given()
-			.basePath("/cozinhas")
-			.port(8080)
 			.accept(ContentType.JSON)
 			.when().get().then()
 			.statusCode(HttpStatus.OK.value());
 	}
+	
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		RestAssured.given()
+			.body(" {\"nome\": \"Chinesa\" } ")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+	}
+	
 }
